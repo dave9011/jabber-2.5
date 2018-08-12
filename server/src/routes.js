6445cf088e1062b37f910579903da881e3faf889
David Hernandez <hrdavidl@gmail.com>
2018-08-12 19:32:00 -0400

    Move register function into an AuthenticationController

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,47 +1,5 @@
+const AuthenticationController = require('./controllers/AuthenticationController');
+
 module.exports = (app, models) => {
-  app.post('/register', async (request, response) => {
-    const body = request.body;
-
-    let error;
-    let user;
-
-    // 1. Attempt to find a user with the given email
-    try {
-      error, user = await models.User.findOne({
-        email: body.email,
-      });
-    } catch (err) {
-      console.log(err);
-
-      response.status(400).send({
-        error: err.message,
-      });
-    }
-
-    // 2. If one was found, we error out b/c the email is already is registered
-    if (user) {
-      response.status(400).send({
-        error: 'This user has already been registered!',
-      });
-    }
-
-    // 3. Attempt to register the user
-    try {
-      error, user = await models.User.create({
-        email: body.email,
-        password: body.password,
-      });
-    } catch (err) {
-      console.log(err);
-
-      response.status(400).send({
-        error: err.message,
-      });
-    }
-
-    response.send({
-      message: 'You user was registered!',
-      user: user,
-    });
-  });
+  app.post('/register', AuthenticationController.register);
 };
